0f5440c9450dde0348b6da970f6e93b5817d1c09
rahmah.abou <rahmah.abouelfadl@edu-fh.joannu>
2025-10-22 13:41:02 +0200

    Initial commit
    lkjkjhkjhkjhkjhjkhkj

---
+++ src/main/java/org/example/klasse1.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class klasse1 {
+    Scanner sc = new Scanner(System.in);
+
+    public void Antworten() {
+        System.out.println("whitney fragt Rahmah zurück:");
+        String r = sc.nextLine();
+    }
+}
